06bccdc7cd95a2c6d1465d1b5eb01c34e8f58435
Andy Maskiell <maskiella@gmail.com>
2022-10-29 10:09:47 -0400

    Successful hello world with some bells and whistles

--- main.py
+++ main.py
@@ -4,14 +4,7 @@ import sys
 from time import sleep
 
 # `hubs` module: https://pybricks.com/ev3-micropython/hubs.html
-try:
-    from ev3dev2.display import Display
-except Exception as e:
-    print(("-" * 30) + " NOPETOWN for pybricks " + ("-" * 30))
-    print(e)
-    # raise e
-
-from pybricks import ev3brick
+from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (
     Motor,
     TouchSensor,
@@ -33,26 +26,35 @@ from pybricks.parameters import (
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 
-from sensor_and_motor_tests import App
 
-# from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
-# from ev3dev2.sensor import INPUT_1
-# from ev3dev2.sensor.lego import TouchSensor
-# from ev3dev2.led import Leds
+ev3brick = EV3Brick()
 
-ev3brick.sound.beep()
-ev3brick.display.clear()
-ev3brick.display.text(f"Voltage is: {ev3brick.battery.voltage()}")
 
-# while callable(ev3brick.buttons) and not ev3brick.buttons():
-#     sleep(10)
+def main():
+    ev3brick.speaker.beep()
+    ev3brick.screen.clear()
+    ev3brick.screen.print("Voltage is: {}".format(ev3brick.battery.voltage()))
+    ev3brick.speaker.say("Bootin up")
+    ev3brick.screen.print("Bootin' up . . .")
+    sleep(1)
+
+    ev3brick.speaker.beep()
+    ev3brick.screen.clear()
+    ev3brick.speaker.say("Bingo Bango")
+    ev3brick.screen.print("BINGO BANGO!")
+    sleep(2)
+
+    # test_motor = Motor(Port.B)
+    # test_motor.run_target(500, 90)
+    sleep(5)
+    ev3brick.speaker.say("Bye forever!")
+    sleep(1)
+
+    # ev3brick.speaker.beep(frequency=1000, duration=500)
 
 
 if __name__ == "__main__":
-    ev3brick.display.text("Bootin' up . . .")
-    sleep(1)
-    ev3brick.display.text("BINGO BANGO!")
-    test_app = App(name="test_app", brick_device=ev3brick)
-    sleep(2)
-    test_app.run()
-    sleep(10)
+    main()
+
+    # while callable(ev3brick.buttons) and not ev3brick.buttons():
+    #     sleep(10)
